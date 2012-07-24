b251ac9e05ac6697218c11700f5a7de61ef946fa
Guy Rixon <guyrixon@gmail.com>
2012-07-24 12:41:34 +0100

    Now handles both application/x-www-url-encoded and multipart/form-data. Latter encoding is preferred, as it support long request-bodies.

--- src/main/java/eu/vamdc/xsams/views/CsvServlet.java
+++ src/main/java/eu/vamdc/xsams/views/CsvServlet.java
@@ -1,18 +1,22 @@
 package eu.vamdc.xsams.views;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringReader;
 import java.net.URL;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  * Servlet to transform an HTML table to CSV. XSLT is used for the transformation.
@@ -65,21 +69,54 @@ public class CsvServlet extends ErrorReportingServlet {
       throws RequestException, IOException, TransformerConfigurationException, TransformerException {
     response.setContentType("text/csv");
     response.setCharacterEncoding("UTF-8");
-    LOG.info(request.getParameter("content"));
     
-    StreamSource in = new StreamSource(new StringReader(request.getParameter("content")));
-    StreamResult out = new StreamResult(response.getWriter());
+    if (request.getContentType().startsWith("application/x-www-form-urlencoded")) {
+      LOG.info("Handling application/x-www-form-urlencoded");
+      StreamSource in = new StreamSource(new StringReader(request.getParameter("content")));
+      StreamResult out = new StreamResult(response.getWriter());
+      getTransformer().transform(in, out);
+    }
+    else if (request.getContentType().startsWith("multipart/form-data")) {
+      LOG.info("Handling multipart/form-data");
+      InputStream stream = null;
+      try {
+        ServletFileUpload upload = new ServletFileUpload();
+        FileItemIterator iter = upload.getItemIterator(request);
+        boolean cached = false;
+        while (iter.hasNext()) {
+          FileItemStream item = iter.next();
+          String name = item.getFieldName();
+          if (name.equals("content") && item.isFormField()) {
+            stream = item.openStream();
+            StreamSource in = new StreamSource(stream);
+            StreamResult out = new StreamResult(response.getWriter());
+            getTransformer().transform(in, out);
+          }
+        }
+      }
+      catch (FileUploadException e) {
+        throw new RequestException(e);
+      }
+      finally {
+        if (stream != null) {
+          stream.close();
+        }
+      }
+    }
+    else {
+      throw new RequestException("Can't read " + request.getContentType() + " input");
+    }
     
-    Transformer t = TransformerFactory.newInstance("net.sf.saxon.TransformerFactoryImpl", null).newTransformer(getXslt());
-    t.transform(in, out);
+    
   }
   
-  protected Source getXslt() {
+  protected Transformer getTransformer() throws TransformerConfigurationException {
     String stylesheetName = getInitParameter("stylesheet");
     URL in = this.getClass().getResource("/"+stylesheetName);
     if (in == null) {
       throw new IllegalStateException("Can't find the stylesheet " + stylesheetName);
     }
-    return new StreamSource(in.toString());
+    StreamSource xslt = new StreamSource(in.toString());
+    return TransformerFactory.newInstance("net.sf.saxon.TransformerFactoryImpl", null).newTransformer(xslt);
   }
 }
